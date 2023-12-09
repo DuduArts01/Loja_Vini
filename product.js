bd2c5be0eaaeaae6e6b5b9cc7b6eb5e99d81ce7f
Eduardo <eduardodemedeirossiqueira@gmail.com>
2023-12-09 16:23:50 -0300

    update_visibility

---
+++ product.js
@@ -0,0 +1,9 @@
+function clickProduct(){
+    if(infoProduct.style.display == "block"){
+        infoProduct.style.display = "none"
+        reserve_space.style.display = "block"
+    } else{
+        infoProduct.style.display = "block"
+        reserve_space.style.display = "none"
+    }
+}
