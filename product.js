e74562e96923b35fc93dd074546023ae8cd0b6dc
Eduardo <eduardodemedeirossiqueira@gmail.com>
2023-12-10 16:09:49 -0300

    choose_file_update

--- product.js
+++ product.js
@@ -1,9 +1,49 @@
+const inputFile = document.querySelector("#picture__input");
+const pictureImage = document.querySelector(".picture__image");
+const pictureImageTxt = "Choose an image";
+pictureImage.innerHTML = pictureImageTxt;
+
 function clickProduct(){
     if(infoProduct.style.display == "block"){
-        infoProduct.style.display = "none"
-        reserve_space.style.display = "block"
+        infoProduct.style.display = "none";
+        reserve_space.style.display = "block";
     } else{
-        infoProduct.style.display = "block"
-        reserve_space.style.display = "none"
+        infoProduct.style.display = "block";
+        reserve_space.style.display = "none";
     }
 }
+
+function clickBuy(){
+    if(box_information.style.display == "block"){
+        box_information.style.display = "none";
+        infoProduct.style.display = "block";
+        pictureImage.innerHTML = pictureImageTxt;
+    } else{
+        box_information.style.display = "block";
+        infoProduct.style.display = "none";
+    }
+}
+
+inputFile.addEventListener("change", function (e) {
+  const inputTarget = e.target;
+  const file = inputTarget.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+    
+    reader.addEventListener("load", function (e) {
+      const readerTarget = e.target;
+
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.classList.add("picture__img");
+
+      pictureImage.innerHTML = "";
+      pictureImage.appendChild(img);
+    });
+
+    reader.readAsDataURL(file);
+  } else {
+    pictureImage.innerHTML = pictureImageTxt;
+  }
+});
--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function clickMenu(){
     if(itens.style.display == "block"){
         itens.style.display = "none"
+        
     } else{
-        itens.style.display = "block"
+        itens.style.display = "block"   
     }
 }
